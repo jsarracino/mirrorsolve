bc470a19cbe0e54e5a605a0f5dba8a9c04aced0b
Tobias Kappé <tobias@kap.pe>
2022-10-21 18:39:51 +0200

    Extend infer_equations to multi-argument functions.

--- tests/Equation.v
+++ tests/Equation.v
@@ -58,38 +58,88 @@ Fixpoint wrap_body (count: nat) (body: term) :=
   match count with
   | 0 => body
   | S count =>
-    tLambda binder_anon hole (wrap_type count body)
+    tLambda binder_anon hole (wrap_body count body)
   end
 .
 
 Fixpoint dummy_args (count: nat) (offset: nat) :=
   match count with
   | 0 => nil
-  | S count => tRel offset :: dummy_args count (S offset)
+  | S count => List.app (dummy_args count (S offset)) [tRel offset]
   end
 .
 
 MetaCoq Quote Definition eq_quoted := @eq.
 MetaCoq Quote Definition eq_refl_quoted := @eq_refl.
 
+Definition tmDebug {A: Type} (thing: A) : TemplateMonad unit :=
+  tmEval all thing >>= tmPrint
+.
+
+(* Custom version of MetaCoq's subst function. Theirs assumes that bound terms
+   within the arguments will need to be shifted within the produced term, but
+   in our case we do not want this to happen.
+
+   Bear in mind that this may be unsound in general because of dependent
+   types --- may the CIC gods have mercy on our souls. *)
+Fixpoint subst_nolift (s : list term) (k : nat) (u : term) : term :=
+  match u with
+  | tRel n =>
+      if PeanoNat.Nat.leb k n
+      then
+       match nth_error s (n - k) with
+       | Some b => b
+       | None => tRel (n - #|s|)
+       end
+      else tRel n
+  | tEvar ev args => tEvar ev (map (subst s k) args)
+  | tCast c kind ty => tCast (subst s k c) kind (subst s k ty)
+  | tProd na A B => tProd na (subst s k A) (subst s (S k) B)
+  | tLambda na T M => tLambda na (subst s k T) (subst s (S k) M)
+  | tLetIn na b ty b' =>
+      tLetIn na (subst s k b) (subst s k ty) (subst s (S k) b')
+  | tApp u0 v => mkApps (subst s k u0) (map (subst s k) v)
+  | tCase ind p c brs =>
+      let k' := #|pcontext p| + k in
+      let p' := map_predicate id (subst s k) (subst s k') p in
+      let brs' := map_branches_k (subst s) k brs in
+      tCase ind p' (subst s k c) brs'
+  | tProj p c => tProj p (subst s k c)
+  | tFix mfix idx =>
+      let k' := #|mfix| + k in
+      let mfix' := map (map_def (subst s k) (subst s k')) mfix in
+      tFix mfix' idx
+  | tCoFix mfix idx =>
+      let k' := #|mfix| + k in
+      let mfix' := map (map_def (subst s k) (subst s k')) mfix in
+      tCoFix mfix' idx
+  | _ => u
+  end.
+
 Definition infer_equation
   (arg_type_quoted: term)
   (func_quoted: term)
   (info: case_info)
   (body: term)
+  (depth: nat)
+  (offset: nat)
   (index: nat)
 :=
   arg_count <- constructor_argument_count arg_type_quoted index ;;
   match func_quoted with
   | tConst (_, func_name) _ =>
-    let claim_lhs := tApp func_quoted
-                          [tApp (tConstruct info.(ci_ind) index [])
-                                (dummy_args arg_count 0)] in
-    let claim_quoted :=
-      wrap_type arg_count (tApp eq_quoted [hole; claim_lhs; body]) in
+    let construct := tApp (tConstruct info.(ci_ind) index [])
+                          (dummy_args arg_count 0) in
+    let args_pre := dummy_args (depth-1-offset) (offset+arg_count) in
+    let args_post := dummy_args offset arg_count in
+    let args := (args_pre ++ [construct] ++ args_post)%list in
+    let claim_lhs := tApp func_quoted args in
+    let claim_rhs := subst_nolift [construct] (offset+arg_count) body in
+    let claim_eq := tApp eq_quoted [hole; claim_lhs; claim_rhs] in
+    let claim_quoted := wrap_type (arg_count+depth-1) claim_eq in
     claim <- tmUnquoteTyped Type claim_quoted ;;
     let proof_quoted :=
-      wrap_body arg_count (tApp eq_refl_quoted [hole; body]) in
+      wrap_body (arg_count+depth-1) (tApp eq_refl_quoted [hole; claim_rhs]) in
     proof <- tmUnquoteTyped claim proof_quoted ;;
     let eqn_base := func_name ++ "_equation_" ++ string_of_nat (index+1) in
     eqn_name <- tmFreshName eqn_base ;;
@@ -99,18 +149,54 @@ Definition infer_equation
   end
 .
 
-Fixpoint infer_equations_inner
+Fixpoint infer_equations_walk_cases
   (arg_type_quoted: term)
   (func_quoted: term)
   (info: case_info)
   (bodies: list (branch term))
+  (depth: nat)
+  (offset: nat)
   (index: nat)
 :=
   match bodies with
   | nil => tmReturn 0
   | body :: bodies =>
-    infer_equation arg_type_quoted func_quoted info body.(bbody) index ;;
-    infer_equations_inner arg_type_quoted func_quoted info bodies (S index)
+    infer_equation arg_type_quoted func_quoted info body.(bbody) depth offset index ;;
+    infer_equations_walk_cases arg_type_quoted func_quoted info bodies depth offset (S index)
+  end
+.
+
+Fixpoint infer_equations_inner
+  (body: term)
+  (top_quoted: term)
+  (context: list term)
+:=
+  match body with
+  | tLambda _ arg_type_quoted body =>
+    infer_equations_inner body top_quoted (arg_type_quoted :: context)
+  | tCase info _ (tRel offset) bodies =>
+    match nth_error context offset with
+    | Some arg_type_quoted =>
+      infer_equations_walk_cases arg_type_quoted top_quoted info bodies (List.length context) offset 0
+    | None =>
+      tmFail "Term contains match on something that is not an argument."
+    end
+  | _ =>
+    tmDebug body ;;
+    tmFail "Symbol body is not a function with a match inside."
+  end
+.
+
+Definition infer_equations_handle_fixpoint
+  (body: term)
+  (func_quoted: term)
+:=
+  match body with
+  | tFix (first_fixpoint :: nil) 0 =>
+    let body := subst10 func_quoted first_fixpoint.(dbody) in
+    infer_equations_inner body func_quoted nil
+  | _ =>
+    infer_equations_inner body func_quoted nil
   end
 .
 
@@ -119,16 +205,11 @@ Definition infer_equations {A: Type} (func: A) :=
   match func_quoted with
   | tConst func_path _ =>
     def <- tmQuoteConstant func_path true ;;
-    match cst_body def with
-    | Some (tLambda _ arg_type_quoted (tCase info _ (tRel 0) bodies)) =>
-      infer_equations_inner arg_type_quoted func_quoted info bodies 0
-    | Some (tFix (first_fixpoint :: nil) 0) =>
-      match subst1 func_quoted 0 first_fixpoint.(dbody) with
-      | tLambda _ arg_type_quoted (tCase info _ (tRel 0) bodies) =>
-        infer_equations_inner arg_type_quoted func_quoted info bodies 0
-      | _ => tmFail "foo"
-      end
-    | _ => tmFail "Symbol body is not a function with a match inside."
+    match def.(cst_body) with
+    | Some body =>
+      infer_equations_handle_fixpoint body func_quoted
+    | None =>
+      tmFail "Function does not seem to have a body."
     end
   | _ => tmFail "Symbol is not a constant."
   end
@@ -157,6 +238,16 @@ Section Tests.
   Check decr_strange_equation_1.
   Check decr_strange_equation_2.
 
+  Definition decr_multi_arg (n m: nat) :=
+    match n with
+    | 0 => m
+    | S n => n
+    end.
+
+  MetaCoq Run (infer_equations decr_multi_arg).
+  Check decr_multi_arg_equation_1.
+  Check decr_multi_arg_equation_2.
+
   Fixpoint decr_rec (n: nat) : nat :=
     match n with
     | 0 => 0
@@ -167,4 +258,23 @@ Section Tests.
   MetaCoq Run (infer_equations decr_rec).
   Check decr_rec_equation_1.
   Check decr_rec_equation_2.
+
+  Fixpoint decr_rec_multi_arg (n m: nat) : nat :=
+    match n with
+    | 0 => m
+    | S n => decr_rec_multi_arg n m
+    end
+  .
+
+  MetaCoq Run (infer_equations decr_rec_multi_arg).
+  Check decr_rec_multi_arg_equation_1.
+  Check decr_rec_multi_arg_equation_2.
+
+  MetaCoq Run (infer_equations Nat.add).
+  Check add_equation_1.
+  Check add_equation_2.
+
+  MetaCoq Run (infer_equations Nat.mul).
+  Check mul_equation_1.
+  Check mul_equation_2.
 End Tests.

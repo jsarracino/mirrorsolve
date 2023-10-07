956d2715f4370b071b4e3356bb697a15280ae4d5
John Sarracino <jsarracino@cornell.edu>
2023-10-07 12:54:30 -0400

    add funs and rels

--- src/theories/PolyFirstOrder.v
+++ src/theories/PolyFirstOrder.v
@@ -58,16 +58,18 @@ Inductive var: forall t_ctx, ctx t_ctx -> ty_expr t_ctx 0 -> Type :=
     var _ (Snoc _ c s) s'.
 Derive Signature NoConfusion for var.
 
+Context (sig: signature).
+
 Inductive tm t_ctx : ctx t_ctx -> ty_expr t_ctx 0 -> Type :=
 | TVar: 
   forall c s,
     var t_ctx c s ->
-    tm t_ctx c s.
-(* | TFun:
+    tm t_ctx c s
+| TFun:
   forall c args ret,
-    sig.(sig_funs) args ret ->
-    HList.t (tm c) args ->
-    tm c ret. *)
+    sig.(sig_funs) t_ctx args ret ->
+    HList.t (tm t_ctx c) args ->
+    tm t_ctx c ret.
 Derive Signature for tm.
 
 Inductive fm t_ctx : ctx t_ctx -> Type :=
@@ -81,11 +83,11 @@ Inductive fm t_ctx : ctx t_ctx -> Type :=
     tm t_ctx c s ->
     fm t_ctx c
 (* A relation (with arguments) *)
-(* | FRel:
-    forall c args,
-      sig.(sig_rels) args ->
-      HList.t (tm c) args ->
-      fm c *)
+| FRel:
+  forall c args,
+    sig.(sig_rels) t_ctx args ->
+    HList.t (tm t_ctx c) args ->
+    fm t_ctx c 
 (* Negation, disjunction, conjunction, and implication *)
 | FNeg: forall c, fm t_ctx c -> fm t_ctx c
 | FOr: forall c, fm t_ctx c -> fm t_ctx c -> fm t_ctx c
@@ -139,7 +141,6 @@ Inductive ty_valu : ty_ctx -> snoc_list Type -> Type :=
 Equations lookup_tc_var ctx tys (tv: ty_valu ctx tys) (tc_var: ty_ctx_var ctx) : Type := {
   lookup_tc_var _ _ (TVSnoc _ T _ _) (TCVHere _) := T; 
   lookup_tc_var _ _ (TVSnoc _ _ _ tv') (TCVThere _ tc_var') := lookup_tc_var _ _ tv' tc_var'; 
-  (* lookup_tc_var _ (TCVThere c _ tc_var) := lookup_tc_var c tc_var; *)
 }.
 
 Equations interp_te_var ctx tys (tv: ty_valu ctx tys) n (te_var: ty_expr_var sig_sorts ctx n) : arity_ftor n := {
@@ -157,6 +158,23 @@ Equations interp_te ctx tys (tv: ty_valu ctx tys) n (te: ty_expr sig_sorts ctx n
       (interp_te ctx tys tv _ l) T;
 }.
 
+Record interp_sig (sig: signature sig_sorts) := {
+  interp_funs: 
+    forall t_ctx ctx args ret, 
+      sig_funs sig_sorts sig t_ctx args ret -> 
+      forall (tv: ty_valu t_ctx ctx), 
+        HList.t (interp_te t_ctx ctx tv 0) args ->
+        interp_te t_ctx ctx tv 0 ret;
+  interp_rels: 
+    forall t_ctx ctx args, 
+      sig_rels sig_sorts sig t_ctx args -> 
+      forall (tv: ty_valu t_ctx ctx), 
+        HList.t (interp_te t_ctx ctx tv 0) args ->
+        Prop;
+}.
+
+Context (model: interp_sig sig).
+
 Inductive valu ty_ctx tys (tv: ty_valu ty_ctx tys) : ctx sig_sorts ty_ctx -> Type := 
 | VEmp : 
   valu ty_ctx tys tv (SLNil _)
@@ -171,15 +189,28 @@ Equations interp_var t_ctx tys tv e_ctx e (env: valu t_ctx tys tv e_ctx) (v: var
   interp_var _ _ _ _ _ (VSnoc _ _ env _) (VThere _ _ _ _ vi) := interp_var _ _ _ _ _ env vi;
 }.
 
-Equations interp_tm t_ctx tys tv e_ctx e (env: valu t_ctx tys tv e_ctx) (t: tm sig_sorts t_ctx e_ctx e) : interp_te t_ctx tys tv 0 e := {
-  interp_tm _ _ _ _ _ env (TVar _ _ v) := interp_var _ _ _ _ _ env v;
+(* Obligation Tactic := idtac. *)
+Equations 
+  interp_tm t_ctx tys tv e_ctx e (env: valu t_ctx tys tv e_ctx) (t: tm sig_sorts sig t_ctx e_ctx e) : interp_te t_ctx tys tv 0 e by struct t := {
+  interp_tm _ _ _ _ _ env (TVar _ _ v) := 
+    interp_var _ _ _ _ _ env v;
+  interp_tm _ _ _ _ _ env (TFun _ _ _ _ _ _ f hargs) := 
+    model.(interp_funs sig) _ _ _ _ f _ (interp_tms _ _ _ _ env _ hargs);
+} where 
+  interp_tms t_ctx ctx tv e_ctx (env: valu t_ctx ctx tv e_ctx) tys (tms: HList.t (tm sig_sorts sig t_ctx e_ctx) tys) : HList.t (interp_te t_ctx ctx tv 0) tys by struct tms := {
+  interp_tms _ _ _ _ _ _ hnil := 
+    hnil;
+  interp_tms _ _ _ _ _ _ (t ::: ts) := 
+    interp_tm _ _ _ _ _ _ t ::: interp_tms _ _ _ _ _ _ ts;
 }.
 
-Equations interp_fm t_ctx tys tv e_ctx (env: valu t_ctx tys tv e_ctx) (f: fm sig_sorts t_ctx e_ctx) : Prop := {
+Equations interp_fm t_ctx tys tv e_ctx (env: valu t_ctx tys tv e_ctx) (f: fm sig_sorts sig t_ctx e_ctx) : Prop := {
   interp_fm _ _ _ _ _ (FTrue _) := True;
   interp_fm _ _ _ _ _ (FFalse _) := False;
   interp_fm _ _ _ _ _ (FEq _ _ l r) := 
     (interp_tm _ _ _ _ _ _ l) = (interp_tm _ _ _ _ _ _ r);
+  interp_fm _ _ _ _ _ (FRel _ _ _ _ _ f hargs) := 
+    model.(interp_rels sig) _ _ _ f _ (interp_tms _ _ _ _ _ _ hargs);
   interp_fm _ _ _ _ _ (FAnd _ l r) := 
     interp_fm _ _ _ _ _ l /\ interp_fm _ _ _ _ _ r ;
   interp_fm _ _ _ _ _ (FOr _ l r) := 
@@ -198,10 +229,10 @@ Equations interp_fm t_ctx tys tv e_ctx (env: valu t_ctx tys tv e_ctx) (f: fm sig
       interp_fm _ _ _ _ (VSnoc _ _ _ _ _ env x) f ;
 }.
 
-Equations interp_pfm t_ctx tys (tv: ty_valu t_ctx tys) (pf: poly_fm sig_sorts t_ctx) : Prop := {
-  interp_pfm t_ctx tys tv (PFm _ _ f) := 
+Equations interp_pfm t_ctx tys (tv: ty_valu t_ctx tys) (pf: poly_fm sig_sorts sig t_ctx) : Prop := {
+  interp_pfm t_ctx tys tv (PFm _ f) := 
     interp_fm t_ctx tys tv _ (VEmp _ _ _) f;
-  interp_pfm t_ctx tys tv (PForall _ _ pf) := 
+  interp_pfm t_ctx tys tv (PForall _ pf) := 
     forall (T: Type), 
       interp_pfm _ _ (TVSnoc _ T _ _) pf;
 }.
